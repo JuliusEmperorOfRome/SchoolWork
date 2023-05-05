9d284d6b91eae6fe541b9388ae453a7dd5e30067
JuliusEmperorOfRome <122451857+JuliusEmperorOfRome@users.noreply.github.com>
2023-05-05 09:53:51 +0300

    Update ex6.cpp

--- p140/ex6.cpp
+++ p140/ex6.cpp
@@ -6,7 +6,7 @@
 
 int main() {
 	std::map<char, unsigned> counts{};
-	std::ifstream in{"text_lt_asci.txt"};
+	std::ifstream in{"text.txt"};
 	for(std::istream::int_type ch; (ch = in.get()) != std::istream::traits_type::eof();) {
 		unsigned char c = static_cast<unsigned char>(ch);	
 		if(std::isalpha(c)) {
