2400ba3a5355f0bbb46956d860633ffb0d75dc1a
JuliusEmperorOfRome <122451857+JuliusEmperorOfRome@users.noreply.github.com>
2023-06-02 08:36:47 +0300

    Update ex1.cpp

--- p151/ex1.cpp
+++ p151/ex1.cpp
@@ -4,10 +4,10 @@
 
 
 auto count_symbol(std::istream& in, char c) {
-  using iter = std::istreambuf_iterator;
+  using iter = std::istreambuf_iterator<char>;
   return std::count(iter{in}, iter{}, c);
 }
 
 int main() {
-  std::cin << count_symbol(std::cout, 'a');
+  std::cout << count_symbol(std::cin, 'a');
 }
