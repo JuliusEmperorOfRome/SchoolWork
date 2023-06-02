31fa7c5e2fec84bab15b0958028874f0048be7d0
JuliusEmperorOfRome <122451857+JuliusEmperorOfRome@users.noreply.github.com>
2023-06-02 13:39:57 +0300

    Update ex3.cpp

--- p151/ex3.cpp
+++ p151/ex3.cpp
@@ -9,15 +9,16 @@ void process(std::istream& in, std::ostream& out) {
   int_t start = 0;
   while(start != str.size()) {
     int_t dot = str.find('.', start);
-
+	if(dot == str.npos) break;
+	
     unsigned space_count = 0;
     for(;start != dot; ++start) {
       if(str[start] == ' ') ++space_count;
     }
-    out << space_count << '\n';
+    out << (space_count + 1) << '\n';
 
     //skip .[space][space]...[space]
-    while(str[++start] == ' ');
+    while(start != str.size() && str[++start] == ' ');
   }
 }
 
